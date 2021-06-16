7ef54ccc83572f9250e05017d55c2985495b8dab
Your Name <jc29advincula@gmail.com>
2021-06-16 17:18:09 +0800

    modify loan type page

--- src/pages/LoanType.js
+++ src/pages/LoanType.js
@@ -1,8 +1,33 @@
+import { Grid, Paper } from "@material-ui/core"
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    height: 250,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 const LoanType = () => {
+  const classes = useStyles();
+
   return ( 
-    <div>Loan Type Page</div>
+    <Grid container spacing={3}>
+      <Grid item md={4}>
+        <Paper className={classes.paper} />
+      </Grid>
+
+      <Grid item md={8}>
+        <Paper className={classes.paper} />
+      </Grid>
+    </Grid>
   )
 }
    
-  export default LoanType
+export default LoanType
   
